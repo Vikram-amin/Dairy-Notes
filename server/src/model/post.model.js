aaaec76d470f957c9159c9b72bbadab273509e5d
Vikram <vikramamin22@gmail.com>
2022-06-17 10:23:48 +0530

    backend model is created

---
+++ server/src/model/post.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const postSchema = mongoose.Schema({
+  title: String,
+
+  message: String,
+
+  creator: String,
+
+  tags: [String],
+
+  selectedFile: String,
+
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+let Post = mongoose.model("post", "postSchema");
+
+module.exports = Post
